c832ba6d830846f37ebac6fdb0f3a180a712112e
24sec <1033272962@qq.com>
2019-04-03 16:27:09 +0800

    Vue

---
+++ my-test/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    {{msg}}
+    <input type="text" v-model="info">
+    <button @click="add">添加</button>
+    <ul>
+      <todo-item :key="val" v-for="val in list">
+        <template v-slot:myitem="itemProps">
+          <span :style="{fontSize:'50px',color:itemProps.checked?'red':'blue'}">{{val}}</span>
+        </template>        
+        
+      </todo-item>
+    </ul>
+    
+  </div>
+  
+</template>
+
+<script>
+import TodoItem from './components/TodoItem.vue'
+export default {
+  name: 'app',
+  components: {
+    TodoItem
+  },
+  data(){
+    return {
+      msg:'hello world',
+      info:'',
+      list:[]
+    }
+  },
+  methods:{
+    add(){
+      this.list.push(this.info);
+      this.info='';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ my-test/src/components/TodoItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <li class="item" >
+      <input type="checkbox" v-model="checked">
+      <slot name="myitem" v-bind="{checked}"></slot>
+  </li>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+        
+            checked:false
+        }
+    }
+}
+</script>
+
+<style scoped>
+.item {
+  color: red;
+}
+</style>
